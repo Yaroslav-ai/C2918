5d597fdbc0fa4ae2866ca677e947d52c6393bfa2
Yaroslav-ai <132147083+Yaroslav-ai@users.noreply.github.com>
2023-05-20 18:54:02 +0300

    super

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+class Hello:
+    def __init__(self):
+        print("Hello")
+
+
+class HelloWorld(Hello):
+    def __init__(self):
+        super().__init__()
+        print("World!")
+
+
+helloworld = HelloWorld()
+
+
+class Computer:
+    def __init__(self):
+        super().__init__()
+        self.memory = 128
+
+class Display:
+    def __init__(self):
+        super().__init__()
+        self.resolution = "4k"
+
+class SmartPhone(Display,Computer):
+    def print_info(self):
+        print(self.resolution)
+        print(self.memory)
+
+iphone = SmartPhone()
+iphone.print_info()
 
